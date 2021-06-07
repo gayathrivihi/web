79805515238fd42ae029a59e4f8cdf9fe64a2bb3
gayathri <n-gayathri.n-gayathri@capgemini.com>
2021-06-07 14:57:43 +0530

    web

---
+++ Max_Avg/Max_Avg/Class3.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Max_Avg
+{
+    class Class3
+    {
+    }
+}
---
+++ Max_Avg/Max_Avg/encapsulation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+	public class Source
+	{
+		public int Add(int a, int b, int c)
+		{
+			return a + b + c;
+		}
+		public double Add(double a, double b, double c)
+		{
+			return a + b + c;
+		}
+		public static void Main(string[] args)
+		{
+			Source s = new Source();
+			int res = s.Add(2, 3, 4);
+			Console.WriteLine(res);
+			double res1 = s.Add(2.5, 4.5, 6.3);
+			Console.WriteLine(res1);
+		}
+	}
+
+
